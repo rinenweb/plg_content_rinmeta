7f4d23db40be216be37e066a77629fe53b0aedbe
Ioannis Fytros <rinenweb@gmail.com>
2023-12-11 14:35:22 +0200

    Add files via upload
    working copy

--- plg_rinmeta/rinmeta.php
+++ plg_rinmeta/rinmeta.php
@@ -9,91 +9,109 @@
 
 defined('_JEXEC') or die;
 
-namespace Joomla\Plugin\Content\Rinmeta\Extension;
-
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Uri\Uri;
 
-final class Rinmeta extends CMSPlugin
+class PlgContentRinmeta extends CMSPlugin
 {
     /**
      * Event method onContentBeforeDisplay
-     *
      * @param   string  $context  The context of the content being passed to the plugin
      * @param   mixed   &$row     An object with a "text" property
      * @param   mixed   &$params  Additional parameters
      * @param   int     $page     Optional page number
-     * @return  null
      */
     public function onContentBeforeDisplay($context, &$row, &$params, $page = 0)
     {
-
-        $twitteraccount     = $this->params->get('twitteraccount', '');
-        $type               = $this->params->get('type', 'summary');
+        $twitterAccount = $this->params->get('twitteraccount', '');
+        $type = $this->params->get('type', 'summary');
 
         if ($context == 'com_content.article') {
-
-            // Load twitter meta tags
-            $this->setMetadata (
-                $this->setMetatitle(
-                    $row->metadata,
-                    $row->title
-                ),
-                $this->setImage(
-                    $row->images,
-                    $row->text
-                ),
-                $this->setMetadesc(
-                    $row->metadesc,
-                    $row->text
-                ),
-                $twitteraccount,
+            // Load Twitter meta tags
+            $this->setTwitterMetadata(
+                $this->setMetatitle($row->metadata, $row->title),
+                $this->setImage($row->images, $row->text),
+                $this->setMetadesc($row->metadesc, $row->text),
+                $twitterAccount,
                 $type
             );
 
+            // Load Open Graph meta tags
+            $this->setOpenGraphMetadata(
+                $this->setMetatitle($row->metadata, $row->title),
+                $this->setImage($row->images, $row->text),
+                $this->setMetadesc($row->metadesc, $row->text),
+                JUri::current()
+            );
         }
     }
 
     /**
-     * Event method that create the open graph meta tags
-     * @param   string  $title  The article's title
+     * Set Twitter meta tags
      */
-    protected function setMetadata ($title, $image, $metadesc, $twitteraccount, $type) {
+    protected function setTwitterMetadata($title, $image, $metadesc, $twitterAccount, $type)
+    {
         $doc = JFactory::getDocument();
-        $doc->setMetaData( 'twitter:card', $type );
-        $doc->setMetaData( 'twitter:site', $twitteraccount );
-        $doc->setMetaData( 'twitter:title', $title );
-        $doc->setMetaData( 'twitter:description', $metadesc );
-        $doc->setMetaData( 'twitter:image', $image );
+        $doc->setMetaData('twitter:card', $type);
+        $doc->setMetaData('twitter:site', $twitterAccount);
+        $doc->setMetaData('twitter:title', $title);
+        $doc->setMetaData('twitter:description', $metadesc);
+        $doc->setMetaData('twitter:image', $image);
+    }
+
+    /**
+     * Set Open Graph meta tags
+     */
+    protected function setOpenGraphMetadata($title, $image, $metadesc, $url)
+    {
+        $doc = JFactory::getDocument();
+        $config = JFactory::getConfig();
+    
+        $doc->setMetaData('og:title', $title);
+        $doc->setMetaData('og:description', $metadesc);
+        $doc->setMetaData('og:image', $image);
+        $doc->setMetaData('og:url', $url);
+        $doc->setMetaData('og:type', 'article'); // You can adjust the type based on your content
+        $doc->setMetaData('og:locale', $config->get('language'));
+        $doc->setMetaData('og:site_name', $config->get('sitename'));
+
+        // Fetch the Facebook App ID from plugin parameters
+        $facebookAppId = $this->params->get('facebookappid', '');
+
+        // Set the Facebook App ID meta tag if a value is provided
+        if (!empty($facebookAppId)) {
+            $doc->setMetaData('fb:app_id', $facebookAppId);
+        }
     }
 
     /**
      * Extract article's image
-     * @return string
      */
-    protected function setImage($images, $text) {
+    protected function setImage($images, $text)
+    {
         $fullImage = json_decode($images);
         preg_match_all('|<img.*?src=[\'"](.*?)[\'"].*?>|i', $text, $matches);
-        if ( !empty($fullImage->image_fulltext) ) {
-            $image = JURI::base() . $fullImage->image_fulltext;
-        } elseif( !empty($matches[ 1 ][ 0 ]) ) {
-            $image = $matches[ 1 ][ 0 ];
-            $image = str_replace(JURI::base(), '', $image);
-            $image = JURI::base() . $image;
+
+        if (!empty($fullImage->image_fulltext)) {
+            $image = JUri::base() . $fullImage->image_fulltext;
+        } elseif (!empty($matches[1][0])) {
+            $image = $matches[1][0];
+            $image = str_replace(JUri::base(), '', $image);
+            $image = JUri::base() . $image;
         } else {
             $image = '';
         }
 
         return $image;
     }
-    
+
     /**
      * Extract article's meta title
-     * @return string
      */
-    protected function setMetatitle($metadata, $title) {
+    protected function setMetatitle($metadata, $title)
+    {
         $metadata = json_decode($metadata);
-        if( !empty($metadata->metatitle) ) {
+        if (!empty($metadata->metatitle)) {
             $metatitle = $metadata->metatitle;
         } else {
             $metatitle = htmlspecialchars($title);
@@ -101,18 +119,30 @@ final class Rinmeta extends CMSPlugin
 
         return $metatitle;
     }
-    
-    /**
-     * Extract article's meta description
-     * @return string
-     */
-    protected function setMetadesc($metadesc, $text) {
-        if($metadesc) {
-            $metadesc = $metadesc;
-        } else {
-            $metadesc = substr( strip_tags($text), 0, 157 ) . '...';
+
+/**
+ * Extract article's meta description
+ */
+protected function setMetadesc($metadesc, $text, $limit = 159)
+    {
+    // If the meta description is provided, use it
+    if ($metadesc) {
+        return $metadesc;
+    }
+
+    // Otherwise, trim the text to the specified character limit without breaking words
+    $text = strip_tags($text);
+    if (strlen($text) > $limit) {
+        $text = substr($text, 0, $limit);
+        $lastSpace = strrpos($text, ' ');
+
+        if ($lastSpace !== false) {
+            $text = substr($text, 0, $lastSpace);
         }
 
-        return $metadesc;
+        $text .= '...';
+    }
+
+    return $text;
     }
 }
